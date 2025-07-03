5839f7ad2d45cccd2fc44258d1a676f7f062eeb8
srushti shinde <218044135+srushti09146@users.noreply.github.com>
2025-07-03 18:17:42 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,17 @@
+
+public class Employee {
+    protected int id;
+    protected int age;
+    protected String name;
+    protected boolean isPermanent;
+
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+
+        
+        emp.age = (int) 35.5;
+
+        System.out.println("Successfully started");
+    }
+}
+
---
+++ Student1.java
@@ -0,0 +1,11 @@
+
+public class Student {
+    private String studentName;
+    private String collegeName;
+    private int studentID;
+
+    public static void main(String[] args) {
+        System.out.println("Successful");
+    }
+}
+
